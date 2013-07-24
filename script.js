84adeb57057a0fcad56485e84d79d19d7dcf0325
toxictoast <toxictoast1@gmail.com>
2013-07-24 13:43:37 -0700

    css stylesheet

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $('div').mouseenter(function() {
+
+      $(this).animate({
+
+          height:'+=10px'
+      });
+  });
+
+  $('div').mouseleave(function() {
+
+    $(this).animate({
+
+        height: '-=10px'
+    });
+  });
+
+  $('div').click(function() {
+
+    $(this).toggle(1000);
+
+  });
+
+});
